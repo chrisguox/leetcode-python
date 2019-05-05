1341e6465c4e1b9e4f3a29fb668e71b25a2502d6
chris guo <chrisguox@gmail.com>
2019-05-05 22:44:33 +0800

    add solution 1

---
+++ s1_two_sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    """
+    利用map(dict)做处理，时间复杂度为 O(n);
+    """
+    def two_sum(self, nums, target):
+        nums_dict = {}
+        for index, num in enumerate(nums):
+            addend = target - num
+            if addend not in nums_dict:
+                nums_dict[num] = index
+                continue
+            return [nums_dict[addend], index]
