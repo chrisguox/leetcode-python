af33225731cc87c9da4bca607437349a26dc9c63
chris guo <chrisguox@gmail.com>
2019-05-07 02:01:13 +0800

    add solution 3

---
+++ s3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    哨兵，记录重复字符的位置，往后继续计算下一个子串；
+    """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sub_string_dict = {}
+        max_len = 0
+        start = -1
+
+        for index, word in enumerate(s):
+            word = ord(word)
+            if word in sub_string_dict and sub_string_dict[word] > start:
+                start = sub_string_dict[word]
+
+            current_max_len = index - start
+            if current_max_len > max_len:
+                max_len = current_max_len
+
+            sub_string_dict[word] = index
+
+        return max_len
