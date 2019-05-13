7bd8e3d2cc08a131a2fb5671b7cbf6a205504a92
Chris Guo <chrisguox@gmail.com>
2019-05-13 23:35:27 +0800

    add solution 9

---
+++ s9_palindrome_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 and (x % 10 != 0 and x != 0):
+            return False
+
+        reverse_int = 0
+        while reverse_int < x:
+            reverse_int = reverse_int * 10 + x % 10
+            x //= 10
+
+        is_palindrome_number = reverse_int == x or reverse_int // 10 == x
+        return is_palindrome_number
